63bbd8e547b7e36781cbabf589909a496d5077ff
Shivam Sharma <meshivam81@gmail.com>
2023-09-24 19:11:12 +0530

    index.js update

--- backend/index.js
+++ backend/index.js
@@ -35,11 +35,14 @@ app.get('/', (req, res) => {
 app.use('/books', booksRoutes);
 
 // Database connection
-mongoose.connect(process.env.DB)
+ await mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => {
 console.log('App has Connected with Database')
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}`);
 });
 })
 .catch((error) => {
